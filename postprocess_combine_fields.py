11eed817859b9fe48f4c48e70f06d8fe54a5f57c
Andrew C. Ross <andrew.c.ross@noaa.gov>
2024-05-23 12:01:23 -0400

    Avoid warnings with newer python

--- postprocess_combine_fields.py
+++ postprocess_combine_fields.py
@@ -4,7 +4,9 @@ import xarray
 
 def process_file(f):
     ds = xarray.open_dataset(f)
-    return ds.swap_dims({'time': 'lead'})
+    # set_coords is needed to avoid later warnings from
+    # concat with newer xarray
+    return ds.swap_dims({'time': 'lead'}).set_coords('init')
 
 
 if __name__ == '__main__':
@@ -32,11 +34,12 @@ if __name__ == '__main__':
         member['lead'].attrs['units'] = 'months'
         member = member.rename({'time': 'verif'})
         member['member'] = e
+        member = member.set_coords('member')
         members.append(member)
 
     print('Concat')
     model_ds = xarray.concat(members, dim='member').sortby('init') # sorting is important for slicing later
-    model_ds = model_ds.drop(['ens', 'verif', 'mstart', 'ystart'], errors='ignore')
+    model_ds = model_ds.drop_vars(['ens', 'verif', 'mstart', 'ystart'], errors='ignore')
     print('Ensemble mean and anomalies')
     ensmean = model_ds.mean('member')
     climo = ensmean.sel(init=slice(f'{first_year}-01-01', f'{last_year}-12-31')).groupby('init.month').mean('init')
