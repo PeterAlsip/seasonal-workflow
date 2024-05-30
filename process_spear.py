30e2f8c222ed68ba6ad5c5c7961b1c5fc6e6b6c4
Andrew C. Ross <andrew.c.ross@noaa.gov>
2024-05-30 16:49:58 -0400

    Fix SPEAR ocean forecast processing to work with changes to config

--- process_spear.py
+++ process_spear.py
@@ -1,7 +1,7 @@
 import datetime as dt
 from glob import glob
 import numpy as np
-from os import path
+import os
 import xarray
 
 
@@ -22,40 +22,38 @@ def slice_ds(ds, xslice, yslice):
     
     slice_dict = {}
     if xslice is not None:
-        x = xslice.split(',')
         for xcoord in ['xh', 'xq', 'xT']:
             if xcoord in ds.coords:
-                slice_dict.update({xcoord: slice(float(x[0]), float(x[1]))})
+                slice_dict.update({xcoord: slice(float(xslice[0]), float(xslice[1]))})
     if yslice is not None:
-        y = yslice.split(',')
         for ycoord in ['yh', 'yq', 'yT']:
             if ycoord in ds.coords:
-                slice_dict.update({ycoord: slice(float(y[0]), float(y[1]))})
+                slice_dict.update({ycoord: slice(float(yslice[0]), float(yslice[1]))})
     return ds.sel(**slice_dict)
 
 
 def process_monthly(root, domain, var, ens=None, xslice=None, yslice=None):
-    files = sorted(glob(path.join(root, f'{domain}.*-*.{var}.nc')))
-    processed = xarray.open_mfdataset(files, preprocess=prepro, combine='nested', concat_dim='start')[var]
+    files = sorted(glob(os.path.join(root, f'{domain}.*-*.{var}.nc')))
+    processed = xarray.open_mfdataset(files, preprocess=prepro, combine='nested', concat_dim='start', chunks=None, parallel=False)[var]
     processed = slice_ds(processed, xslice, yslice)
 
     if ens != 'pp_ensemble':
-        fname = path.join(root, f'{domain}.monthly_mean.ens_{int(ens):02d}.{var}.nc')
+        fname = os.path.join(root, f'{domain}.monthly_mean.ens_{int(ens):02d}.{var}.nc')
     else:
-        fname = path.join(root, f'{domain}.monthly_mean.ensmean.{var}.nc')
+        fname = os.path.join(root, f'{domain}.monthly_mean.ensmean.{var}.nc')
     processed.to_netcdf(fname)
     print(fname)
 
 
 def process_daily(root, domain, var, ens=None, xslice=None, yslice=None):
-    files = sorted(glob(path.join(root, f'{domain}.*-*.{var}.nc')))
-    processed = xarray.open_mfdataset(files, preprocess=prepro, combine='nested', concat_dim='start')[var]
+    files = sorted(glob(os.path.join(root, f'{domain}.*-*.{var}.nc')))
+    processed = xarray.open_mfdataset(files, preprocess=prepro, combine='nested', concat_dim='start', chunks=None, parallel=False)[var]
     processed = slice_ds(processed, xslice, yslice)
 
     if ens != 'pp_ensemble':
-        fname = path.join(root, f'{domain}.daily_mean.ens_{int(ens):02d}.{var}.nc')
+        fname = os.path.join(root, f'{domain}.daily_mean.ens_{int(ens):02d}.{var}.nc')
     else:
-        fname = path.join(root, f'{domain}.daily_mean.ensmean.{var}.nc')
+        fname = os.path.join(root, f'{domain}.daily_mean.ensmean.{var}.nc')
     processed.to_netcdf(fname)
     print(fname)
 
@@ -64,18 +62,19 @@ if __name__ == '__main__':
     import argparse
     from yaml import safe_load
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--domain')
-    parser.add_argument('-f', '--freq')
-    parser.add_argument('-v', '--var')
-    parser.add_argument('-e', '--ensemble')
+    parser.add_argument('-r', '--root', default=os.environ['TMPDIR'])
+    parser.add_argument('-d', '--domain', required=True)
+    parser.add_argument('-f', '--freq', required=True)
+    parser.add_argument('-v', '--var', required=True)
+    parser.add_argument('-e', '--ensemble', required=True)
     parser.add_argument('-c', '--config', default=None)
     args = parser.parse_args()
 
     if args.config is not None:
         with open(args.config, 'r') as file: 
             config = safe_load(file)
-            xslice = (config['domain']['westlon'], config['domain']['eastlon'])
-            yslice = (config['domain']['southlat'], config['domain']['northlat'])
+            xslice = (config['domain']['west_lon'], config['domain']['east_lon'])
+            yslice = (config['domain']['south_lat'], config['domain']['north_lat'])
     else:
         xslice = yslice = None
 
--- process_spear_job.sh
+++ process_spear_job.sh
@@ -3,13 +3,15 @@
 #SBATCH --job-name=process_spear
 #SBATCH --time=2880
 #SBATCH --partition=batch
-#SBATCH --output=logs/%j.out
+#SBATCH --output=logs/spear_%j.out
 
 # Wrapper to run workflow to extract and combine SPEAR forecasts.
 # Should be sbatch-ed,
 # but running more than a few at a time seems to cause
 # failures during dmget. 
 
+# Newer prebuilt python envs
+# are missing dask and/or yaml
 module load python/3.9
 module load gcp
     
@@ -22,6 +24,6 @@ set config=$5
 set files=`python spear_path.py -d $domain -f $freq -v $var -e $ens -c $config`
 dmget $files
 gcp $files $TMPDIR/
-set processed=`python process_spear.py $TMPDIR -d $domain -f $freq -v $var -e $ens -c $config`
-gcp $processed /work/$USER/spear/processed/
+set processed=`python process_spear.py -r $TMPDIR -d $domain -f $freq -v $var -e $ens -c $config`
+gcp $processed /work/$USER/spear/forecast_output_data/
 
