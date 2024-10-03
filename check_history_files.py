929ba492c788c93019209f38c6b762e7c4dae009
Andrew C. Ross <andrew.c.ross@noaa.gov>
2024-10-03 08:52:41 -0400

    Check whether history files transferred correctly

---
+++ check_history_files.py
@@ -0,0 +1,37 @@
+import argparse
+from yaml import safe_load
+from postprocess_extract_fields import ForecastRun
+
+def colorprint(msg, color):
+    colors = {
+        'ok': '\033[92m',
+        'warning': '\033[93m',
+        'fail': '\033[91m',
+        'end': '\033[0m'
+    }
+    print(colors[color] + msg + colors['end'])
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--config', type=str, required=True)
+args = parser.parse_args()
+with open(args.config, 'r') as file: 
+    config = safe_load(file)
+
+first_year = config['forecasts']['first_year']
+last_year = config['forecasts']['last_year']
+nens = config['forecasts']['ensemble_size']
+template = config['filesystem']['forecast_history']
+
+for ystart in range(first_year, last_year+1):
+    for mstart in config['forecasts']['months']:
+        for ens in range(1, nens+1):
+            run = ForecastRun(ystart=ystart, mstart=mstart, ens=ens, template=template)
+            tar = run.archive_dir / run.tar_file
+            if tar.is_file():
+                colorprint(f'{run.tar_file} e{ens:02d}: found', 'ok')
+            else:
+                if tar.with_suffix(tar.suffix + '.gcp').is_file():
+                    colorprint(f'{run.tar_file} e{ens:02d}: partial transfer', 'warning')
+                else:
+                    colorprint(f'{run.tar_file} e{ens:02d}: not found', 'fail')
