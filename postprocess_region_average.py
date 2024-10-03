3dfb4dd41f4046e81d6659cbad57a9bf6c288227
Andrew C. Ross <andrew.c.ross@noaa.gov>
2024-10-03 08:47:52 -0400

    Calculate and write region averages after combining forecasts

---
+++ postprocess_region_average.py
@@ -0,0 +1,36 @@
+import os
+from pathlib import Path
+import xarray
+
+
+if __name__ == '__main__':
+    import argparse
+    from yaml import safe_load
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config', type=str, required=True)
+    parser.add_argument('-d', '--domain', type=str, default='ocean_month')
+    parser.add_argument('-v', '--var', type=str, required=True)
+    parser.add_argument('-m', '--mean', action='store_true', help='Include only ensemble mean in combined result, dropping individual members.')
+    args = parser.parse_args()
+    with open(args.config, 'r') as file: 
+        config = safe_load(file)
+    model_output_data = Path(config['filesystem']['model_output_data'])
+
+    fname = f'forecasts_{args.domain}_{args.var}_ensmean.nc' if args.mean else f'forecasts_{args.domain}_{args.var}.nc'
+    ds = xarray.open_dataset(model_output_data / fname)
+
+    masks = xarray.open_dataset(config['regions']['mask_file'])
+    if 'yh_sub01' in ds and 'xh_sub01' in ds:
+        ds = ds.rename({f'{v}_sub01': v for v in ['yh', 'xh']})
+
+    averages = []
+    for reg in config['regions']['names']:
+        print(reg)
+        weights = masks['areacello'].where(masks[reg]).fillna(0)
+        ave = ds.weighted(weights).mean(['yh', 'xh']).load()
+        ave['region'] = reg
+        averages.append(ave)
+
+    averages = xarray.concat(averages, dim='region')
+    outname = f'forecasts_{args.domain}_{args.var}_ensmean_regionmean.nc' if args.mean else f'forecasts_{args.domain}_{args.var}_regionmean.nc'
+    averages.to_netcdf(model_output_data / outname)
