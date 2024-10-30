bba65ca82e616afaea979928b675d5d0f4d34ad8
Andrew C. Ross <andrew.c.ross@noaa.gov>
2024-10-30 09:09:23 -0400

    Move to functions

--- postprocess_combine_fields.py
+++ postprocess_combine_fields.py
@@ -5,6 +5,72 @@ import xarray
 from utils import smooth_climatology
 
 
+def process_var(var, config, cmdargs):
+    first_year = config['climatology']['first_year']
+    last_year = config['climatology']['last_year']
+    nens = config['forecasts']['ensemble_size']
+    tmp = Path(os.environ['TMPDIR'])
+    model_output_data = Path(config['filesystem']['model_output_data'])
+    members = []
+
+    if cmdargs.mean:
+        # Large files: ensemble average, then concatenate averages
+        for m in config['forecasts']['months']:
+            for y in range(config['forecasts']['first_year'], config['forecasts']['last_year']+1):
+                month_file = tmp / f'{cmdargs.domain}_{var}_{y}_{m:02d}_ensmean.nc'
+                files = list((model_output_data / 'extracted' / cmdargs.domain).glob(f'{y}-{m:02d}-e??.{cmdargs.domain}.nc'))
+                if len(files) == 1: # single ensemble member
+                    run(f'ncks -v {var} -h {files[0].as_posix()} -O {month_file}', shell=True, check=True)
+                    members.append(month_file)
+                    print(month_file)
+                elif len(files) > 1: 
+                    file_str = ' '.join(map(lambda x: x.as_posix(), files))
+                    run(f'ncea -v {var} -h {file_str} -O {month_file}', shell=True, check=True)
+                    members.append(month_file)  
+                    print(month_file)
+    else:
+        # Regular files: concatenate initializations together
+        for e in range(1, nens+1):
+            out_file = tmp / f'{cmdargs.domain}_{var}_e{e:02d}.nc'
+            if not out_file.exists() or cmdargs.rerun:
+                files = list((model_output_data / 'extracted' / cmdargs.domain).glob(f'????-??-e{e:02d}.{cmdargs.domain}.nc'))
+                if len(files) > 0:
+                    file_str = ' '.join(map(lambda x: x.as_posix(), files))
+                    print(f'ncrcat {len(files)} files to {out_file}')
+                    run(f'ncrcat -v {var},member -h {file_str} -O {out_file}', shell=True, check=True)
+            members.append(out_file)
+
+    concat_dim = 'init' if cmdargs.mean else 'member'
+    print(f'Concat by {concat_dim}')
+    model_ds = xarray.open_mfdataset(members, combine='nested', concat_dim=concat_dim).sortby('init') # sorting is important for slicing later
+    model_ds = model_ds.drop_vars(['ens', 'verif', 'mstart', 'ystart'], errors='ignore').load()
+    print('Ensemble mean and anomalies')
+    if cmdargs.mean:
+        ensmean = model_ds
+    else:
+        ensmean = model_ds.mean('member')
+    climo = ensmean[var].sel(init=slice(f'{first_year}-01-01', f'{last_year}-12-31')).groupby('init.month').mean('init')
+    if 'daily' in cmdargs.domain:
+        print('Smoothing daily climatology')
+        climo = smooth_climatology(climo, dim='lead')
+    anom = model_ds.groupby('init.month') - climo
+    anom = anom.rename({v: f'{v}_anom' for v in anom.data_vars})
+    model_ds = xarray.merge([model_ds, anom])
+    # Write the climatology, being sure that appropriate coords are ints.
+    # Also trying to remove the empty dimension "time" from the output.
+    encoding = {v: {'dtype': 'int32'} for v in ['lead', 'month']}
+    climo.encoding = {}
+    print('Writing climatology')
+    climo.to_netcdf(model_output_data / f'climatology_{cmdargs.domain}_{var}_{first_year}_{last_year}.nc',
+        encoding=encoding)
+    # Do the same for the full set of forecasts
+    encoding = {v: {'dtype': 'int32'} for v in ['lead', 'member', 'month'] if v in model_ds}
+    print('Writing forecasts')
+    fname = f'forecasts_{cmdargs.domain}_{var}_ensmean.nc' if cmdargs.mean else f'forecasts_{cmdargs.domain}_{var}.nc'
+    model_ds.to_netcdf(model_output_data / fname, encoding=encoding)
+
+
+
 if __name__ == '__main__':
     import argparse
     from yaml import safe_load
@@ -17,68 +83,11 @@ if __name__ == '__main__':
     args = parser.parse_args()
     with open(args.config, 'r') as file: 
         config = safe_load(file)
-    tmp = Path(os.environ['TMPDIR'])
-
-    model_output_data = Path(config['filesystem']['model_output_data'])
-    model_output_data.mkdir(exist_ok=True)
-    first_year = config['climatology']['first_year']
-    last_year = config['climatology']['last_year']
-    nens = config['forecasts']['ensemble_size']
-
-    members = []
-
-    if args.mean:
-        # Large files: ensemble average, then concatenate averages
-        for m in config['forecasts']['months']:
-            for y in range(config['forecasts']['first_year'], config['forecasts']['last_year']+1):
-                month_file = tmp / f'{args.domain}_{args.var}_{y}_{m:02d}_ensmean.nc'
-                files = list((model_output_data / 'extracted' / args.domain).glob(f'{y}-{m:02d}-e??.{args.domain}.nc'))
-                if len(files) == 1: # single ensemble member
-                    run(f'ncks -v {args.var} -h {files[0].as_posix()} -O {month_file}', shell=True, check=True)
-                    members.append(month_file)
-                    print(month_file)
-                elif len(files) > 1: 
-                    file_str = ' '.join(map(lambda x: x.as_posix(), files))
-                    run(f'ncea -v {args.var} -h {file_str} -O {month_file}', shell=True, check=True)
-                    members.append(month_file)  
-                    print(month_file)
+    Path(config['filesystem']['model_output_data']).mkdir(exist_ok=True)
+    if ',' in args.var:
+        cmdvar = args.var.split(',')
+        for v in cmdvar:
+            process_var(v, config, args)
     else:
-        # Regular files: concatenate initializations together
-        for e in range(1, nens+1):
-            out_file = tmp / f'{args.domain}_{args.var}_e{e:02d}.nc'
-            if not out_file.exists() or args.rerun:
-                files = list((model_output_data / 'extracted' / args.domain).glob(f'????-??-e{e:02d}.{args.domain}.nc'))
-                if len(files) > 0:
-                    file_str = ' '.join(map(lambda x: x.as_posix(), files))
-                    print(f'ncrcat {len(files)} files to {out_file}')
-                    run(f'ncrcat -v {args.var},member -h {file_str} -O {out_file}', shell=True, check=True)
-            members.append(out_file)
+        process_var(args.var, config, args)
 
-    concat_dim = 'init' if args.mean else 'member'
-    print(f'Concat by {concat_dim}')
-    model_ds = xarray.open_mfdataset(members, combine='nested', concat_dim=concat_dim).sortby('init') # sorting is important for slicing later
-    model_ds = model_ds.drop_vars(['ens', 'verif', 'mstart', 'ystart'], errors='ignore').load()
-    print('Ensemble mean and anomalies')
-    if args.mean:
-        ensmean = model_ds
-    else:
-        ensmean = model_ds.mean('member')
-    climo = ensmean[args.var].sel(init=slice(f'{first_year}-01-01', f'{last_year}-12-31')).groupby('init.month').mean('init')
-    if 'daily' in args.domain:
-        print('Smoothing daily climatology')
-        climo = smooth_climatology(climo, dim='lead')
-    anom = model_ds.groupby('init.month') - climo
-    anom = anom.rename({v: f'{v}_anom' for v in anom.data_vars})
-    model_ds = xarray.merge([model_ds, anom])
-    # Write the climatology, being sure that appropriate coords are ints.
-    # Also trying to remove the empty dimension "time" from the output.
-    encoding = {v: {'dtype': 'int32'} for v in ['lead', 'month']}
-    climo.encoding = {}
-    print('Writing climatology')
-    climo.to_netcdf(model_output_data / f'climatology_{args.domain}_{args.var}_{first_year}_{last_year}.nc',
-        encoding=encoding)
-    # Do the same for the full set of forecasts
-    encoding = {v: {'dtype': 'int32'} for v in ['lead', 'member', 'month'] if v in model_ds}
-    print('Writing forecasts')
-    fname = f'forecasts_{args.domain}_{args.var}_ensmean.nc' if args.mean else f'forecasts_{args.domain}_{args.var}.nc'
-    model_ds.to_netcdf(model_output_data / fname, encoding=encoding)
--- postprocess_region_average.py
+++ postprocess_region_average.py
@@ -1,22 +1,11 @@
-import os
 from pathlib import Path
 import xarray
 
 
-if __name__ == '__main__':
-    import argparse
-    from yaml import safe_load
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--config', type=str, required=True)
-    parser.add_argument('-d', '--domain', type=str, default='ocean_month')
-    parser.add_argument('-v', '--var', type=str, required=True)
-    parser.add_argument('-m', '--mean', action='store_true', help='Include only ensemble mean in combined result, dropping individual members.')
-    args = parser.parse_args()
-    with open(args.config, 'r') as file: 
-        config = safe_load(file)
+def process_var(var, config, cmdargs):
     model_output_data = Path(config['filesystem']['model_output_data'])
 
-    fname = f'forecasts_{args.domain}_{args.var}_ensmean.nc' if args.mean else f'forecasts_{args.domain}_{args.var}.nc'
+    fname = f'forecasts_{cmdargs.domain}_{var}_ensmean.nc' if cmdargs.mean else f'forecasts_{cmdargs.domain}_{var}.nc'
     ds = xarray.open_dataset(model_output_data / fname)
 
     masks = xarray.open_dataset(config['regions']['mask_file'])
@@ -29,8 +18,27 @@ if __name__ == '__main__':
         weights = masks['areacello'].where(masks[reg]).fillna(0)
         ave = ds.weighted(weights).mean(['yh', 'xh']).load()
         ave['region'] = reg
+        ave = ave.set_coords('region')
         averages.append(ave)
 
     averages = xarray.concat(averages, dim='region')
-    outname = f'forecasts_{args.domain}_{args.var}_ensmean_regionmean.nc' if args.mean else f'forecasts_{args.domain}_{args.var}_regionmean.nc'
+    outname = f'forecasts_{cmdargs.domain}_{var}_ensmean_regionmean.nc' if cmdargs.mean else f'forecasts_{cmdargs.domain}_{var}_regionmean.nc'
     averages.to_netcdf(model_output_data / outname)
+
+if __name__ == '__main__':
+    import argparse
+    from yaml import safe_load
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config', type=str, required=True)
+    parser.add_argument('-d', '--domain', type=str, default='ocean_month')
+    parser.add_argument('-v', '--var', type=str, required=True)
+    parser.add_argument('-m', '--mean', action='store_true', help='Include only ensemble mean in combined result, dropping individual members.')
+    args = parser.parse_args()
+    with open(args.config, 'r') as file: 
+        config = safe_load(file)
+    if ',' in args.var:
+        cmdvar = args.var.split(',')
+        for v in cmdvar:
+            process_var(v, config, args)
+    else:
+        process_var(args.var, config, args)
