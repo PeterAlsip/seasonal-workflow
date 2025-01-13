f24f180481901acc8f711166b5a9c78d412ea720
Andrew C. Ross <andrew.c.ross@noaa.gov>
2025-01-13 17:35:30 -0500

    Require output directory too

--- analysis_setup/atmos/era5_lp.py
+++ analysis_setup/atmos/era5_lp.py
@@ -2,12 +2,10 @@ from pathlib import Path
 from subprocess import run
 
 
-def main(tp_file, sf_file, tmpdir, outdir):
+def main(tp_file, sf_file, outdir):
     tp_str = tp_file.as_posix()
     sf_str = sf_file.as_posix()
-    if outdir is None:
-        outdir = tp_file.parent
-    lp_str = (tmpdir / tp_file.name.replace('tp', 'lp')).as_posix()
+    lp_str = (outdir / tp_file.name.replace('tp', 'lp')).as_posix()
     cmd = f'cdo -setrtoc,-1e9,0,0 -chname,tp,lp -sub {tp_str} {sf_str} {lp_str}'
     print(cmd)
     run(cmd, shell=True, check=True)
@@ -17,6 +15,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', '--tp', required=True)
     parser.add_argument('-s', '--sf', required=True)
-    parser.add_argument('-o', '--out', default=None)
+    parser.add_argument('-o', '--out', required=True)
     args = parser.parse_args()
-    main(Path(args.tp), Path(args.sf), args.out)
+    main(Path(args.tp), Path(args.sf), Path(args.out))
